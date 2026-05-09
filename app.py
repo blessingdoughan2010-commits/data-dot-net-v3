a6fea2a7ffde9b4f176d33e94478059f560723eb
blessingdoughan2010-commits <blessingdoughan2010@gmail.com>
2026-05-09 13:45:08 +0000

    Enhance app.py with string template for HTML
    Added a string template for rendering HTML directly.

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template_string, send_from_directory
 import os
 
 app = Flask(__name__, static_folder='public')
 
+# We use a string template so you don't need a separate templates folder on your phone
+HTML_CODE = """
+"""
+
 @app.route('/')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/<path:path>')
-def static_files(path):
-    return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
